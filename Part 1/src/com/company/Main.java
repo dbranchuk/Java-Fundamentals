89bbc4bc28e62bb1a0b7a02a07af6abb25065600
Dima Branchuk <branchuk.d@gmail.com>
2020-10-23 20:10:15 +0300

    added task 1

---
+++ Part 1/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a,b,c;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input 'a' ");
+        a = scan.nextDouble();
+        System.out.println("Input 'b' ");
+        b = scan.nextDouble();
+        System.out.println("Input 'c' ");
+        c = scan.nextDouble();
+        double z = ( (a - 3) * b / 2 ) + c;
+        System.out.println("Result = " + z);
+    }
+}
